6e8952d957ed1e223d50dcc45d05967eb7613180
Mahima Jyoti <mahijyoti883@gmail.com>
2026-07-23 12:10:17 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,25 @@
+class KthLargest {
+public:
+priority_queue<int,vector<int>,greater<int>>pq;
+int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(auto c:nums){
+            pq.push(c);
+            while(pq.size()>k){
+                pq.pop();
+            }
+        }
+     
+        
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>k){
+            pq.pop();
+        }
+       return  pq.top();
+        
+    }
+};
